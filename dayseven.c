9b9e960c71a805efb132ed050cee7d0329c216fb
Jack Boyd <j.william.boyd@gmail.com>
2025-03-06 12:10:15 +0000

    Finish day seven part 1

--- dayseven.c
+++ dayseven.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 #include <ctype.h>
 
 #define MAX_LINE_LENGTH 1024
+
 
 int read_file(const char* filename, int*** equations, int** equation_col_counts, long** answers, int* rows) {
   FILE *file = fopen(filename, "r");
@@ -18,22 +20,25 @@ int read_file(const char* filename, int*** equations, int** equation_col_counts,
 
   while(fgets(line, sizeof(line), file)) {
     int cols = 0;
-    char* data = strchr(line, ':');
+    char *data = strchr(line, ':');
     if (data == NULL) {
       perror("Memory allocation failed");
       fclose(file);
       return EXIT_FAILURE;
     } 
+    
     data++; 
     while (*data && isspace((unsigned char)*data)) {
       data++;
     }
+    
     char *line_copy = strdup(data);
     if (!line_copy) {
       perror("Memory allocation failed");
       fclose(file);
       return EXIT_FAILURE;
     }
+    
     char *token = strtok(line_copy, " ");
     while (token) {
       cols++;
@@ -113,8 +118,47 @@ cleanup:
   return EXIT_FAILURE;
 }
 
-int calculate_total_calibration(int*** equations, int** equation_col_counts, long** answers, int rows) {
-   //TODO
+long calculate_total_calibration(int*** equations, int** equation_col_counts, long** answers, int rows) {
+  long total_calibration = 0;
+  int *operations = NULL;
+  for (int i = 0; i < rows; i++) {
+    //0 is + and 1 is *
+    int operation_count = (*equation_col_counts)[i] - 1;
+    if (i == 0) {
+      operations = calloc(operation_count, sizeof(int));
+      if (operations == NULL) {
+        perror("realloc failed");
+        exit(EXIT_FAILURE);
+      }
+    }
+    else {
+      int *tmp = realloc(operations, operation_count * sizeof(int));
+      if (tmp == NULL) {
+        free(operations);
+        perror("realloc failed");
+        exit(EXIT_FAILURE);
+      }
+      operations = tmp; 
+    }
+    bool match = false;
+    for (int mask = 0; (mask < (1 << operation_count)) && !match; mask++) {
+      for (int j = operation_count - 1; j >= 0; j--) {
+        operations[j] = (mask >> j) & 1;
+      }
+      long curr_calibation = (*equations)[i][0];
+      for (int j = 0; j < operation_count; j++) {
+        if (operations[j] == 0)
+          curr_calibation += (*equations)[i][j + 1];
+        else 
+          curr_calibation *= (*equations)[i][j + 1];
+      }
+      if ((*answers)[i] == curr_calibation) { 
+        total_calibration += (*answers)[i];
+        match = true;
+      }
+    }
+  }
+  return total_calibration;
 }
 
 int main() {
@@ -126,6 +170,7 @@ int main() {
   if (read_file("dayseven.txt", &equations, &equation_col_counts, &answers, &rows) == EXIT_FAILURE)
     return EXIT_FAILURE;
 
-  int total_calibration = calculate_total_calibration(&equations, &equation_col_counts, &answers, rows);
+  long total_calibration = calculate_total_calibration(&equations, &equation_col_counts, &answers, rows);
+  printf("%ld\n", total_calibration);
   return 0;
 }
